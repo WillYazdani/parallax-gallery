c41e608e25fe2714e6668dc4c1383b8cb55f8e7e
Will Yazdani <yazd0006@gmail.com>
2022-08-10 14:31:39 -0500

    adding parallax function

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
 import './App.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import {useState} from 'react';
 
 function App() {
+
+  //parallax on mouse movement
+  const [mouseX, setMouseX] = useState(0);
+  const [mouseY, setMouseY] = useState(0);
+  
+  const onMouseMove = (e) => {
+    setMouseX(e.clientX);
+    setMouseY(e.clientY);
+  }
+
   return (
-    <section className='animated-grid'>
-      <div className='card'>a</div>
-      <div className='card'>b</div>
-      <div className='card'>c</div>
-      <div className='card'>d</div>
-      <div className='card'>e</div>
-      <div className='card'>f</div>
-      <div className='card'>g</div>
-      <div className='card'>h</div>
-      <div className='card'>i</div>
-      <div className='card'>j</div>
-      <div className='card'>k</div>
-      <div className='card'>l</div>
-      <div className='card'>main</div>
-    </section>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" />
+      </Routes>
+      <section className='animated-grid'>
+        <div className='card'>A</div>
+        <div className='card'>B</div>
+        <div className='card'>C</div>
+        <div className='card'>D</div>
+        <div className='card'>E</div>
+        <div className='card'>F</div>
+        <div className='card'>G</div>
+        <div className='card'>H</div>
+        <div className='card'>I</div>
+        <div className='card'>J</div>
+        <div className='card'>K</div>
+        <div className='card'>L</div>
+        <div className='card'>PARALLAX GALLERY</div>
+      </section>
+    </BrowserRouter>
   );
 }
 
