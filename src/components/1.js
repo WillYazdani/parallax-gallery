e277e4b16cb1c08e4539a150778ecb15aaf7fab3
Will Yazdani <yazd0006@gmail.com>
2022-08-19 12:59:10 -0500

    started styling individual pages

--- src/components/1.js
+++ src/components/1.js
@@ -1,10 +1,17 @@
 import './tilt.css';
+import { Link } from 'react-router-dom';
+import {  motion, useAnimation } from "framer-motion";
 
 export default function One () {
     console.log('One');
     return(
         <div>
-            <h1>One</h1>
+            <Link to='/'>
+                <motion.button
+                className='home-btn'>
+                    HOME
+                </motion.button>
+            </Link>
         </div>
     )
 }
